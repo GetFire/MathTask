2ec73ae640214d106019537afe98019e24817c19
Denis Azhymov <azhimov93@gmail.com>
2016-10-27 11:13:20 +0300

    Initial commit

---
+++ ОкруглениеЧисла.java
@@ -0,0 +1,13 @@
+
+public class ОкруглениеЧисла {
+    public static void main(String[] args) {
+        double a;
+        a = 4.5;
+    double n = a -(int)a;
+        System.out.print(a+ " округляем и получается - ");
+        if (n>=0.5){
+a++;   }
+else a = (int) a;
+System.out.print((int)a);
+}
+}
