bc85f054ef030054ad28ad3451f4dba2d760d222
Denis Azhymov <azhimov93@gmail.com>
2016-10-27 11:17:44 +0300

    initial commit

--- ОкруглениеЧисла.java
+++ ОкруглениеЧисла.java
@@ -2,7 +2,7 @@
 public class ОкруглениеЧисла {
     public static void main(String[] args) {
         double a;
-        a = 4.5;
+        a = 4.5; // тут вводим любое вещественное число
     double n = a -(int)a;
         System.out.print(a+ " округляем и получается - ");
         if (n>=0.5){
